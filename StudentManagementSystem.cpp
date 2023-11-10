07882aa5614765091f175f5d6f61d77229d687eb
THEMANNICHOLAS <stingyblue@gmail.com>
2023-11-09 19:31:25 -0500

    Remove GPA.h class and file.

--- StudentManagementSystem.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "GPA.h"
-using namespace std;
-int main() {
-    gpa student1('A', 'B', 'A', 'A');
-    cout << "Student GPA = " << student1.calculateGPA();
-    return 0;
-}
